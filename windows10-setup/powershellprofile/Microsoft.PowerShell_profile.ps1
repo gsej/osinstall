3290750de3fe3b5e605566d81519a79ef24695d6
Gareth Erskine-Jones <gsej@erskinejones.com>
2023-10-23 02:37:26 +0100

    update powershell profile

--- windows10-setup/powershellprofile/Microsoft.PowerShell_profile.ps1
+++ windows10-setup/powershellprofile/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,33 @@
+$env:path += ";c:\local\bin\"
+$env:path += ";c:\local\bin\SysinternalsSuite"
+$env:path += ";c:\Program Files\Notepad++"
+$env:path += ";C:\Users\gerskine-Jones\AppData\Roaming\npm"
+
+$env:PSModulePath += ";C:\Users\gerskine-Jones\OneDrive - CFC Underwriting\Documents\PowerShell\Modules"
+
 Import-Module posh-git
 
-$env:Path += ";C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64"
-$env:Path += ";C:\Program Files\Notepad++"
+function Open-Notes {
+&"c:\Program Files\Microsoft VS Code\code.exe" c:\local\repos\notes
+}
+Set-Alias -name notes -value Open-Notes
+
+function GitGraphLog {
+     &"git" log --oneline --decorate --graph
+}
+Set-Alias -name glt -value GitGraphLog
+
+function GitGraphLogAll {
+     &"git" log --oneline --decorate --graph --all
+}
+Set-Alias -name glta -value GitGraphLogAll
+
+$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
 
 
+Set-Alias -name ss -value Select-String
+Set-Alias -name vim -value nvim
 
- function Open-Notes { 
- 
-& "C:\Program Files\Microsoft VS Code\code.exe" c:\local\repos\notes
- 
- }
 
-Set-Alias -name notes -value Open-Notes  		
-
-$env:DOTNET_CLI_TELEMETRY_OPTOUT=1
+Set-PSReadLineOption -PredictionSource None
+$PSStyle.FileInfo.Directory="`e[0m"
