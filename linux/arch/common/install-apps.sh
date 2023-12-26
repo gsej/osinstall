cb5a66a161562a9220f5ed802a70849a642fb82d
Gareth Erskine-Jones <gsej@erskinejones.com>
2023-12-26 17:30:29 +0000

    update

--- linux/arch/common/install-apps.sh
+++ linux/arch/common/install-apps.sh
@@ -132,5 +132,11 @@ yay -S virtualbox virtualbox-host-modules-arch virtualbox-guest-iso
 
 yay -S storageexplorer
 
+
+# pacman stuff
 yay -S informant # prevents upgrade unless archnews has been read
 yay -S rebuild-detector
+
+
+
+yay -S btop
