f1f7c9769d4f77ebea4920f772891d1ba731d06b
Gareth Erskine-Jones <gsej@erskinejones.com>
2023-12-28 21:13:40 +0000

    remove i3, add sway

--- i3config/install-i3.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p ~/.config/systemd/user
-cp plasma-custom-wm.service ~/.config/systemd/user/
-systemctl --user mask plasma-kwin_x11.service
-systemctl --user enable plasma-custom-wm.service
--- i3config/uninstall-i3.sh
+++
@@ -1,3 +0,0 @@
-systemctl --user disable plasma-custom-wm.service
-systemctl --user unmask plasma-kwin_x11.service
-rm ~/.config/systemd/user/plasma-custom-wm.service
